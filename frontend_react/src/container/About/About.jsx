8dedad4fb63b435fe9e0e7c599f110d8f3a99e73
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-05-20 17:08:21 +0700

    connect sanity and react, add abouts to cms

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import {images} from '../../constants/index';
-
 import './About.scss';
-
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01},
-  { title: 'Mobile App Development', description: 'I am a good mobile app developer.', imgUrl:images.about02},
-  { title: 'ERP Development', description: 'I am a good ERP developer, especially Odoo.', imgUrl:images.about03},
-  { title: 'Full Stack', description: 'I am a good full stack devevloper.', imgUrl:images.about04}
-];
+import { urlFor, client } from '../../client';
 
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+
+  }, []);
+
+
   return (
     <>
       <h2 className='head-text'>
@@ -30,7 +34,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
